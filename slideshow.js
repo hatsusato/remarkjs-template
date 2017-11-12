1366c210309b9acfd6a14e29b32fcc19a7ab5c82
hatsusato <knightspm5@gmail.com>
2017-11-12 16:33:14 +0900

    Add slideshow.js

---
+++ slideshow.js
@@ -0,0 +1,28 @@
+var options = {
+    sourceUrl: 'slide.md',
+    highlightStyle: 'googlecode',
+    highlightSpans: false,
+    ratio: '16:9',
+    navigation: {
+        scroll: false,
+        touch: false,
+        click: false,
+    }
+};
+function createSlide() {
+    return remark.create(options);
+};
+function createKatexSlide() {
+    var renderMath = function() {
+        renderMathInElement(document.body, {
+            delimiters: [ // mind the order of delimiters(!?)
+                {left: "$$", right: "$$", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "\\(", right: "\\)", display: false},
+            ]});
+    };
+    return remark.create(options, renderMath);
+}
+var slideshow = createSlide();
+// var slideshow = createKatexSlide();
