27cc21a0f7f16c6874ee49112f7f6068c2ee7762
hatsusato <knightspm5@gmail.com>
2018-05-18 16:08:19 +0900

    Add slide.js

---
+++ slide.js
@@ -0,0 +1,12 @@
+var options = {
+    highlightStyle: 'googlecode',
+    highlightSpans: true,
+    highlightLines: true,
+    ratio: '16:9',
+    navigation: {
+        scroll: false,
+        touch: false,
+        click: false,
+    }
+};
+var slideshow = remark.create(options);
