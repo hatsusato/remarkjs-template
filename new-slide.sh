ddd4a3134b0e57143e98bab040fe81fc65fc94bd
hatsusato <knightspm5@gmail.com>
2018-05-19 17:14:20 +0900

    Renew new-slide.sh

---
+++ new-slide.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+link() {
+    src_path="$1"
+    dst_path="$2"
+    dst_dir=$(dirname "${dst_path}")
+    mkdir -p "${dst_dir}"
+    ln -frsv "${src_path}" "${dst_path}"
+}
+copy() {
+    src_path="$1"
+    dst_path="$2"
+    cp -irv "${src_path}" "${dst_path}"
+}
+
+top_dir=$(cd "$(dirname "$0")"; pwd -P)
+current_dir=$(pwd -P)
+target_dir="${current_dir}/$1"
+index_name='index.html'
+mkdir "${target_dir}"
+link "${top_dir}/js" "${target_dir}/js"
+link "${top_dir}/css" "${target_dir}/css"
+copy "${top_dir}/${index_name}" "${target_dir}/index.html"
