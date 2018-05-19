244a6686244c1de39415601fb0e0c170977cd815
hatsusato <knightspm5@gmail.com>
2018-05-19 16:26:42 +0900

    Fix update.sh

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+set -eu
+
 readonly katex_api_url='https://api.github.com/repos/Khan/KaTeX/releases/latest'
 readonly katex_archive='katex.tar.gz'
 readonly remark_url='https://remarkjs.com/downloads/remark-latest.min.js'
 readonly remark_file='remark-latest.min.js'
 readonly targets=('katex.min.css' 'katex.min.js' 'auto-render.min.js')
+readonly atexit_err='failed to clean: please remove extracted files manually'
 
 atexit() {
-    local error_msg='failed to clean: please remove extracted files manually'
     if test -f "${katex_archive}"; then
         local prefix=$(tar -tf "${katex_archive}" | cut -f 1 -d / | uniq)
         if [[ "${prefix}" =~ $'\n' ]]; then
-            echo "${error_msg}" >&2
+            echo "${atexit_err}" >&2
         elif test -d "${prefix}"; then
             rm -r "${prefix}"
         fi
