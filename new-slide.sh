2426d268911af64d9ddb69147eeb46e40a598147
hatsusato <knightspm5@gmail.com>
2018-06-25 16:47:51 +0900

    Simplify index_name

--- new-slide.sh
+++ new-slide.sh
@@ -18,10 +18,9 @@ copy() {
 top_dir=$(cd "$(dirname "$(realpath "$0")")"; pwd -P)
 current_dir=$(pwd -P)
 target_dir="${current_dir}/$1"
-index_name='index.html'
 mkdir "${target_dir}"
 link "${top_dir}/js" "${target_dir}/js"
 link "${top_dir}/css" "${target_dir}/css"
 link "${top_dir}/fonts" "${target_dir}/fonts"
 copy "${top_dir}/main.css" "${target_dir}/main.css"
-copy "${top_dir}/${index_name}" "${target_dir}/index.html"
+copy "${top_dir}/index.html" "${target_dir}/index.html"
