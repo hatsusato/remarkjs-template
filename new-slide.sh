803f5c68efc2a703377849973d811d749321b302
hatsusato <knightspm5@gmail.com>
2019-11-08 14:41:18 +0900

    Update index.html

--- new-slide.sh
+++ new-slide.sh
@@ -19,7 +19,13 @@ top_dir=$(cd "$(dirname "$(realpath "$0")")"; pwd -P)
 current_dir=$(pwd -P)
 target_dir="${current_dir}/$1"
 mkdir "${target_dir}"
-link "${top_dir}/js" "${target_dir}/js"
-link "${top_dir}/css" "${target_dir}/css"
-link "${top_dir}/fonts" "${target_dir}/fonts"
-copy "${top_dir}/index.html" "${target_dir}/index.html"
+link "${top_dir}"/js "${target_dir}"/js
+link "${top_dir}"/css "${target_dir}"/css
+link "${top_dir}"/fonts "${target_dir}"/fonts
+copy "${top_dir}"/index.html "${target_dir}"/index.html
+copy "${top_dir}"/css/reset.css "${target_dir}"/reset.css
+copy "${top_dir}"/css/main.css "${target_dir}"/main.css
+copy "${top_dir}"/css/title.css "${target_dir}"/title.css
+copy "${top_dir}"/css/common.css "${target_dir}"/common.css
+copy "${top_dir}"/js/remark-latest.min.js "${target_dir}"/remark-latest.min.js
+copy "${top_dir}"/js/slide.js "${target_dir}"/slide.js
